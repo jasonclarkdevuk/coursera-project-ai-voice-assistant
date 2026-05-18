848853d1166bb07223c834a9402b0a837563074a
jasonclarkdevuk <jasonclarkdev@gmail.com>
2026-05-18 15:12:28 -0400

    Implement STT function in worker using IBM Watson STT API

--- README.md
+++ README.md
@@ -5,3 +5,5 @@ A voice assistant using OpenAI and IBM Watson Speech Libraries for Embed.
 A virtual assistant that can take voice input, convert it to text using speech-to-text technology, send the text to OpenAI's GPT-3 model, receive a response, convert it to speech using text-to-speech technology and finally play it back to the user.
 
 The voice assistant will have a responsive front-end using HTML, CSS, and JavaScript, and a reliable back-end using Flask.
+
+Note: There should be a certs folder that contains the rootCA.cert however it has not been pushed to this repoistory.
--- worker.py
+++ worker.py
@@ -3,10 +3,37 @@ import requests
 
 openai_client = OpenAI()
 
-
+# Take in audio data received from the browser and pass to IBM Watson STT
+# Return the transcription of the audio data
 def speech_to_text(audio_binary):
-    return None
+    # Set up Watson SST HTTP API URL
+    base_url = "https://sn-watson-stt.labs.skills.network"
+    api_url = base_url + "/speech-to-text/api/v1/recognize"
 
+    # Set up parameters for request
+    # Use US English model for processing speech
+    parameters = {
+        "model": "en-US-_Multimedia"
+    }
+
+    # Set up request body - sending audio data in body of POSt request
+    body = audio_binary
+
+    # Send HTTP POST request
+    # Convert response to JSON
+    response = requests.post(api_url, params=params, data=audio_binary)
+
+    # Parse response
+    text = null
+    if response.get("results"):
+        latest_result = response.get("results")[-1]
+        latest_text = latest_result.get("alternatives")[-1].get("transcript")
+
+        print("Recognised Text: ", latest_text)
+
+        text = latest_text
+    
+    return text
 
 def text_to_speech(text, voice=""):
     return None
